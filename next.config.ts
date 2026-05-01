f493cbefd879dc58f3bbea59c8a203529acb701a
artemmakarenko <artemmakarenko88@gmail.com>
2026-05-01 14:04:09 +0300

    Allow HMR from LAN dev origin
    Add allowedDevOrigins entries so Next.js dev resources (webpack-hmr) can be accessed from the LAN host 192.168.0.251.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://192.168.0.251",
+    "http://192.168.0.251:3000",
+    "http://192.168.0.251:3001",
+  ],
 };
 
 export default nextConfig;
